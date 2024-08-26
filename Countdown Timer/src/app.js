fda802d08668e9d3e0718d8b235e5762add913a2
Cory545 <coryburton545@gmail.com>
2024-08-26 11:58:03 +0100

    Start Countdown Timer Project

---
+++ Countdown Timer/src/app.js
@@ -0,0 +1,7 @@
+//Grab ids from the index.html
+const hourDisplay = document.getElementById("hourDisplay");
+const minuteDisplay = document.getElementById("minuteDisplay");
+const secondDisplay = document.getElementById("secondDisplay");
+const pauseButton = document.getElementById("pauseButton");
+const resetButton = document.getElementById("resetButton");
+const playButton = document.getElementById("playButton");
