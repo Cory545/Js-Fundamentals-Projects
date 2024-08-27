a33a612dfa11f3c3fbf7dfe42d368f08e5989415
Cory545 <coryburton545@gmail.com>
2024-08-27 10:06:26 +0100

    Finish Countdown Timer V1

--- Countdown Timer/src/app.js
+++ Countdown Timer/src/app.js
@@ -1,7 +1,33 @@
 //Grab ids from the index.html
-const hourDisplay = document.getElementById("hourDisplay");
-const minuteDisplay = document.getElementById("minuteDisplay");
-const secondDisplay = document.getElementById("secondDisplay");
+const countdownDisplay = document.getElementById("countdownDisplay");
 const pauseButton = document.getElementById("pauseButton");
 const resetButton = document.getElementById("resetButton");
 const playButton = document.getElementById("playButton");
+const resumeButton = document.getElementById("resumeButton");
+//Variables
+let countdownDisplayNumber = 10;
+let originalCountdownNumber = 10;
+countdownDisplay.innerText = "Countdown from 10";
+let isPaused = false;
+//Functions
+function pauseCountdown(){
+    return isPaused = true;
+};
+function resumeCountdown(){
+    return isPaused = false;
+};
+function resetCountdown(){
+    isPaused = true;
+    return countdownDisplayNumber = originalCountdownNumber;
+};
+function playCountdown(){
+    const intervalId = setInterval(() =>  {
+        if(!isPaused){
+         countdownDisplay.innerText = countdownDisplayNumber;
+        countdownDisplayNumber--;
+
+        if (countdownDisplayNumber < 0){
+            clearInterval(intervalId);
+        }}
+    }, 1000)
+};
