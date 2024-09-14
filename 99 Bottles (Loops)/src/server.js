146926fe9e820996607ac99ba3cb7586e8138e76
Cory545 <coryburton545@gmail.com>
2024-09-14 13:56:51 +0100

    Finish 99 Bottles loop

---
+++ 99 Bottles (Loops)/src/server.js
@@ -0,0 +1,9 @@
+
+function counting(){
+for(let i = 100; i >= 0; i--){
+    let n = i;
+    console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down, pass it around, now there's " + n + " more bottles of beer on the wall.");
+};
+};
+
+counting();
